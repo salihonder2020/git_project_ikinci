2ebb3cbf5cd86c83b7098498b4b1acc3a0ff1c2c
salihonder2020 <69275983+salihonder2020@users.noreply.github.com>
2020-08-09 22:33:11 +0200

    Update Deneme.java

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -5,6 +5,8 @@ public class Deneme {
 	public static void main(String[] args) {
 	System.out.println("Ikinci git projemm");
 		System.out.println("github da degistirdim");
+		
+		System.out.println("github da conflict");
 
 	}
 
